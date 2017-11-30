22324fe6324b620c458fc154de897c2fd0491347
knwng <ken.qrose@gmail.com>
2017-11-30 16:34:07 +0800

    add gitignore

--- extract_RGB_feats.py
+++ extract_RGB_feats.py
@@ -38,8 +38,8 @@ def main():
     caffe_root = '/data2/wqian/caffe-master/'
     vgg_model = os.path.join(caffe_root, 'models/VGG/VGG_ILSVRC_16_layers.caffemodel')
     vgg_deploy = os.path.join(caffe_root, 'models/VGG/VGG_ILSVRC_16_layers_deploy.prototxt')
-    video_path = './data/YouTubeClips'
-    video_save_path = './data/rgb_feats'
+    video_path = './data/YouTubeClips_test'
+    video_save_path = './data/rgb_feats_test'
     videos = os.listdir(video_path)
     videos = filter(lambda x: x.endswith('avi'), videos)
 
